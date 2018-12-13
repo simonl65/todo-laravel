a91894eb82a69ff826deed8cadd5c1e91f29bb2e
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-12-13 10:14:54 +0000

    feat: Add CORS capability.

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+
+    'supportsCredentials' => false,
+    'allowedOrigins' => [
+        'http://localhost:8080',
+        'https://localhost:8080',
+    ],
+    'allowedOriginsPatterns' => [],
+    'allowedHeaders' => ['*'],
+    'allowedMethods' => ['*'],
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+
+];
